7f42094cd618cef800ebb383ec07ba4fb5dcad65
a_szczepanik <a.szczepanik@adform.com>
2022-04-01 12:05:30 +0200

    Create instances of Objects by copying previous values

--- src/main/scala/RobotSimulator.scala
+++ src/main/scala/RobotSimulator.scala
@@ -12,11 +12,23 @@ case object Course {
 
 case class Robot(direction: Course, position: (Int, Int)) {
 
+  import Course._
+
   def advance: Robot = ???
 
-  def turnRight: Robot = ???
+  def turnLeft: Robot = direction match {
+    case North => copy(West)
+    case South => copy(East)
+    case East => copy(North)
+    case West => copy(South)
+  }
 
-  def turnLeft: Robot = ???
+  def turnRight: Robot = direction match {
+    case North => copy(East)
+    case South => copy(West)
+    case East => copy(South)
+    case West => copy(North)
+  }
 
   def robotSimulator(course: String): Robot = {
     course.toList.foldLeft(this) {
