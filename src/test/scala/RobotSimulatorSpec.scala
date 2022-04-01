6007af567394afb5c42a634b07f9f632ff0fa4ea
a_szczepanik <a.szczepanik@adform.com>
2022-04-01 11:52:00 +0200

    Initialize test for Robot Simulator

---
+++ src/test/scala/RobotSimulatorSpec.scala
@@ -0,0 +1,16 @@
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class RobotSimulatorSpec extends AnyFlatSpec with should.Matchers {
+
+  "robotSimulator" should "move right" in {
+  }
+
+  it should "move left" in {
+  }
+
+  it should "advance" in {
+  }
+
+}
