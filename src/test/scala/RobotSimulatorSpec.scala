35388c7e43d7f108d3295d40ead8cfefa609427c
a_szczepanik <a.szczepanik@adform.com>
2022-04-01 12:45:19 +0200

    Complete tests for RobotSimulator methods

--- src/test/scala/RobotSimulatorSpec.scala
+++ src/test/scala/RobotSimulatorSpec.scala
@@ -1,4 +1,4 @@
-import Course.{East, North, West}
+import Course.{East, North, South, West}
 import org.scalatest._
 import flatspec._
 import matchers._
@@ -17,6 +17,42 @@ class RobotSimulatorSpec extends AnyFlatSpec with should.Matchers {
 
   it should "return advance" in {
     robotNorth.advance shouldBe RobotSimulator(North, (0, 1))
+  }
+
+  "robotSimulator.robotSouth" should "move left" in {
+    robotSouth.turnLeft shouldBe RobotSimulator(East, (0, 0))
+  }
+
+  it should "move right" in {
+    robotSouth.turnRight shouldBe RobotSimulator(West, (0, 0))
+  }
+
+  it should "return advance" in {
+    robotSouth.advance shouldBe RobotSimulator(South, (1, 0))
+  }
+
+  "robotSimulator.robotEast" should "move left" in {
+    robotEast.turnLeft shouldBe RobotSimulator(North, (0, 0))
+  }
+
+  it should "move right" in {
+    robotEast.turnRight shouldBe RobotSimulator(South, (0, 0))
+  }
+
+  it should "return advance" in {
+    robotEast.advance shouldBe RobotSimulator(East, (0, -1))
+  }
+
+  "robotSimulator.robotWest" should "move left" in {
+    robotWest.turnLeft shouldBe RobotSimulator(South, (0, 0))
+  }
+
+  it should "move right" in {
+    robotWest.turnRight shouldBe RobotSimulator(North, (0, 0))
+  }
+
+  it should "return advance" in {
+    robotWest.advance shouldBe RobotSimulator(West, (-1, 0))
   }
 
 }
